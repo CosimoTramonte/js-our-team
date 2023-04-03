046ce1b3088d82b2e24914aa84c7006127adc875
CosimoTramonte <cosimotramonte7@gmail.com>
2023-04-03 14:34:37 +0200

    array team and print in console

---
+++ README.md
@@ -0,0 +1,11 @@
+Our Team
+===
+
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+
+## Steps
+- Creare l’array di oggetti con le informazioni fornite.
+- Stampare su console, per ogni membro del team, le sue informazioni.
+
---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+const team = [
+    {
+        imgProfile: "img/wayne-barnett-founder-ceo.jpg",
+        fullName: "Wayne Barnett",
+        role: "Founder & CEO",
+    },
+    {
+        imgProfile: "img/angela-caroll-chief-editor.jpg",
+        fullName: "Angela Caroll",
+        role: "Chief Editor",
+    },
+    {
+        imgProfile: "img/walter-gordon-office-manager.jpg",
+        fullName: "Walter Gordon",
+        role: "Office Manager",
+    },
+    {
+        imgProfile: "img/angela-lopez-social-media-manager.jpg",
+        fullName: "Angela Lopez",
+        role: "Social Media Manager",
+    },
+    {
+        imgProfile: "img/scott-estrada-developer.jpg",
+        fullName: "Scott Estrada",
+        role: "Developer",
+    },
+    {
+        imgProfile: "img/barbara-ramos-graphic-designer.jpg",
+        fullName: "Barbara Ramos",
+        role: "Graphic Designer",
+    }
+];
+
+console.log("array dei membri del team",team);
+
+for(let member of team){
+    console.log("singolo membro del team",member);
+}
